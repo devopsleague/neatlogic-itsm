cba95d266605cd7d39c98fd31138a21d26b87ca5
lvzk <897706680@qq.com>
2024-11-14 09:36:39 +0800

    [功能]工单中心分类优化 #[1289021903306752]工单中心分类优化 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1289021903306752

--- src/main/resources/neatlogic/resources/process/changelog/2024-11-13/neatlogic_tenant.sql
+++ src/main/resources/neatlogic/resources/process/changelog/2024-11-13/neatlogic_tenant.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `process_workcenter`
 ADD COLUMN `fcd` timestamp(3) NULL COMMENT '创建时间' AFTER `thead_config_hash`,
 ADD COLUMN `fcu` char(32) NULL COMMENT '创建人' AFTER `fcd`,
-ADD COLUMN `is_active` tinyint(1) NULL DEFAULT 0 COMMENT '是否禁用' AFTER `fcu`;
+ADD COLUMN `is_active` tinyint(1) NULL DEFAULT 1 COMMENT '是否禁用' AFTER `fcu`;
